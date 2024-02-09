fb3fdac35fcf052f557eadd05fd228320877cee7
Samyak-J7 <sj21samyak@gmail.com>
2024-02-09 23:52:47 +0530

    main

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
 let nav = document.querySelector(".sidebar");
 const shownav = () => {nav.style.display="block";}
 const closebar = () => {nav.style.display="none";}
+function showDiv(index) {
+    document.querySelector("#hoverdiv").style.display = "flex";
+    let divs = document.querySelectorAll("#hoverdiv div");
+    divs.forEach(element => {
+        element.style.display = "none";
+    });    
+    let div_to_show = document.querySelectorAll(`.${index}`);
+    div_to_show.forEach(element => {
+        element.style.display = "flex";
+    });
+}
+function hidedivs() {
+    let divs = document.querySelectorAll("#hoverdiv div");
+    divs.forEach(element => {
+        element.style.display = "none";
+    });    
+    document.querySelector("#hoverdiv").style.display = "none";
+}
+function showdiv() {
+    document.querySelector("#hoverdiv").style.display = "flex";
+}
